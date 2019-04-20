eb4c8b9730fe0f075968071c0e735884a1057bf4
Piotr <piotr5370@gmail.com>
2019-04-20 15:42:47 +0200

    Added moved snake

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import time
+import random
+
 import pygame
 
 # My modules
@@ -22,38 +25,48 @@ class PlayerClass:
     PLAYER_Pos = x, y = 200, 40
     player_x = 200
     player_y = 40
-    SPEED = 40
+    SPEED = 10
 
     def __init__(self, engine, screen):
-        self.spawn(engine, screen)
+        self.snake = self.spawn(engine, screen)
 
     def spawn(self, engine, screen):
         snake = engine.draw.circle(screen, (255, 18, 18), [self.player_x, self.player_y], 20)
         return snake
+
+    @staticmethod
+    def move(speed=0):
+        render.Rendering(pygame, SCREEN, DARK_GREEN, LIGHT_GREEN, WHITE)
+        pygame.draw.circle(SCREEN, (255, 18, 18), (snake.player_x + speed, snake.player_y + speed), 20)
+        pygame.display.flip()
 
     def moveup(self):
         if self.player_y <= 0:
             self.player_y = 0
         else:
             self.player_y -= self.SPEED
+            pygame.display.update(self.snake)
 
     def movedown(self):
-        if self.player_y >= SIZE_WINDOW[1] - self.player_y:
-            self.player_y = SIZE_WINDOW[1] - self.player_y
+        if self.player_y >= SIZE_WINDOW[1]+800 - self.player_y:
+            self.player_y = SIZE_WINDOW[1]+800 - self.player_y
         else:
             self.player_y += self.SPEED
+            pygame.display.update(self.snake)
 
     def moveleft(self):
-        if self.player_y <= 0:
-            self.player_y = 0
+        if self.player_x <= 0:
+            self.player_x = 0
         else:
-            self.player_y -= self.SPEED
+            self.player_x -= self.SPEED
+            pygame.display.update(self.snake)
 
     def moveright(self):
-        if self.player_x >= SIZE_WINDOW[0] - self.player_x:
-            self.player_x = SIZE_WINDOW[0] - self.player_x
+        if self.player_x >= SIZE_WINDOW[0]+800 - self.player_x:
+            self.player_x = SIZE_WINDOW[0]+800 - self.player_x
         else:
             self.player_x += self.SPEED
+            pygame.display.update(self.snake)
 
 
 SCREEN = pygame.display.set_mode(SIZE_WINDOW)
@@ -63,20 +76,19 @@ snake = PlayerClass(pygame, SCREEN)
 pygame.display.flip()
 
 while not STATUS:
-    clock.tick(60)
-    key_pressed = pygame.key.get_pressed()
-
-    if key_pressed[pygame.K_w] or key_pressed[pygame.K_UP]:
-        snake.moveup()
-    if key_pressed[pygame.K_s] or key_pressed[pygame.K_DOWN]:
-        snake.movedown()
-    if key_pressed[pygame.K_a] or key_pressed[pygame.K_LEFT]:
-        snake.moveleft()
-    if key_pressed[pygame.K_d] or key_pressed[pygame.K_RIGHT]:
-        snake.moveright()
-
-while 1:
+    clock.tick(10)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             STATUS = True
-    pygame.display.update()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                snake.moveup()
+            elif event.key == pygame.K_DOWN:
+                snake.movedown()
+            elif event.key == pygame.K_LEFT:
+                snake.moveleft()
+            elif event.key == pygame.K_RIGHT:
+                snake.moveright()
+            snake.move()
+
+pygame.quit()
