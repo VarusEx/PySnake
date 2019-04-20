84ca1b0c1b008348c43f97c11b905011eb278812
Piotr <piotr5370@gmail.com>
2019-04-20 11:17:20 +0200

    Added Snake first position

--- main.py
+++ main.py
@@ -9,17 +9,19 @@ pygame.init()
 # Screen settings
 SIZE_WINDOW = width, height = 800, 800
 RECTENGLES = []
+PLAYER = x, y = 200, 40
 SCREEN = None
 STATUS = False
 
 # Definitions colors
 LIGHT_GREEN = (146, 255, 20)
 DARK_GREEN = (58, 221, 19)
+WHITE = (255, 255, 255)
 
 SCREEN = pygame.display.set_mode(SIZE_WINDOW)
 clock = pygame.time.Clock()
 
-utils.reset_scene(pygame, SCREEN, DARK_GREEN, LIGHT_GREEN, RECTENGLES)
+scene_utils = utils.reset_scene(pygame, SCREEN, DARK_GREEN, LIGHT_GREEN, WHITE, RECTENGLES)
 
 pygame.display.flip()
 
@@ -29,5 +31,4 @@ while not STATUS:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             STATUS = True
-
 
--- utils.py
+++ utils.py
@@ -1,9 +1,11 @@
-def spawn_snake():
-    pass
+def spawn_snake(engine, screen, x=0, y=0):
+    snake = engine.draw.circle(screen, (255, 18, 18), [200 + x, 40 + y], 20)
+    return snake
 
 
-def move():
-    pass
+def move(engine, snake, pos_x, pos_y):
+    snake.move([pos_x, pos_y])
+    engine.display.update(snake)
 
 
 def set_background(screen, color_bg=(255, 255, 255)):
@@ -21,23 +23,24 @@ def draw_rectengle(engine, pos_x, pos_y, screen, color, loops=5, case=1):
             y += 0
         else:
             x += 160
-        rectengles += [rect, x, y]
-        print(rect, x, y)
+        rectengles += [rect]
     return rectengles
 
 
-def reset_scene(engine, screen, color_1, color_2, container):
+def reset_scene(engine, screen, color_1, color_2, color_bg, container):
     i, x, y = 1, 0, 0
-    bg = set_background(screen, color_2)
+    bg = set_background(screen, color_bg)
     for i in range(0, 6):
         container += draw_rectengle(engine, 0, y, screen, color_1, loops=5)
+        container += draw_rectengle(engine, 80, y, screen, color_2, loops=5, case=2)
         y += 160
         if y == 800:
             y = 0
     for i in range(0, 6):
         container += draw_rectengle(engine, 80, y + 80, screen, color_1, loops=5, case=2)
+        container += draw_rectengle(engine, 0, y + 80, screen, color_2, loops=5)
         y += 160
         if y == 800:
             y = 0
 
-    return container, bg
+    return bg, spawn_snake(engine, screen)
