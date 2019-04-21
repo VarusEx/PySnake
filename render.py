4cc9d9a6f3e7b6f32a0973b9c525cd7c00f925a2
Piotr <piotr5370@gmail.com>
2019-04-22 00:38:36 +0200

    Small refactor and add func redraw_background

--- render.py
+++ render.py
@@ -1,35 +1,44 @@
+import pygame
 
-class Rendering:
+
+class Rendering(pygame.Surface):
     RECTENGLES = []
+    SCREEN = None
 
-    def __init__(self, engine, screen, color_1, color_2, color_bg):
-        self.reset_scene(engine, screen, color_1, color_2, color_bg, self.RECTENGLES)
+    def __init__(self, screen, color_1, color_2, color_bg):
+        super().__init__((800, 800))
+        self.SCREEN = screen
+        self.reset_scene(color_1, color_2, color_bg, self.RECTENGLES)
 
-    def reset_scene(self, engine, screen, color_1, color_2, color_bg, container):
+    def reset_scene(self, color_1, color_2, color_bg, container):
         i, x, y = 1, 0, 0
-        bg = self.set_background(screen, color_bg)
+        bg = self.set_background(color_bg)
         for i in range(0, 6):
-            container += self.draw_rectengle(engine, 0, y, screen, color_1, loops=5)
-            container += self.draw_rectengle(engine, 80, y, screen, color_2, loops=5, case=2)
+            container += self.draw_rectengle(0, y, color_1, loops=5)
+            container += self.draw_rectengle(80, y, color_2, loops=5, case=2)
             y += 160
             if y == 800:
                 y = 0
         for i in range(0, 6):
-            container += self.draw_rectengle(engine, 80, y + 80, screen, color_1, loops=5, case=2)
-            container += self.draw_rectengle(engine, 0, y + 80, screen, color_2, loops=5)
+            container += self.draw_rectengle(80, y + 80, color_1, loops=5, case=2)
+            container += self.draw_rectengle(0, y + 80, color_2, loops=5)
             y += 160
             if y == 800:
                 y = 0
         return bg
 
-    def set_background(self, screen, color_bg=(255, 255, 255)):
-        return screen.fill(color_bg)
+    def set_background(self, color_bg=(255, 255, 255)):
+        return self.SCREEN.fill(color_bg)
+    @staticmethod
+    def redraw_background(screen):
+        s = screen.copy()
+        screen.blit(source=s, dest=(0, 0))
 
-    def draw_rectengle(self, engine, pos_x, pos_y, screen, color, loops=5, case=1):
+    def draw_rectengle(self, pos_x, pos_y, color, loops=5, case=1):
         rectengles = []
         i, x, y = 1, pos_x, pos_y
         while i in range(0, loops + 1):
-            rect = engine.draw.rect(screen, color, [x, y, 80, 80])
+            rect = pygame.draw.rect(self.SCREEN, color, [x, y, 80, 80])
             i += 1
             if case == 1:
                 x += 160
